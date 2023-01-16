dec1282ff39332a7f3d6f336fe9d276d5e871b8c
vasanthshanmugam <32473491+vasanthshanmugam@users.noreply.github.com>
2023-01-17 02:54:27 +0530

    Commit project

---
+++ scripts/jsAction_071ab6fc-e80b-487c-83e8-3df6395a60aa.js
@@ -0,0 +1,9 @@
+﻿//Importing package
+var appObj = new Packages.io.perfwise.demo.App();
+try {
+context.currentVU.put("demoObj", appObj); // Exporting variable to context
+var result = appObj.add(5,5); // Accessing the method inside app class
+logger.info("THE OUTPUT IS :::: " + result); //Logging the output
+} catch (exception) {
+logger.debug("Exception occurred ::: \n " + exception);
+}
---
+++ scripts/jsVariable_66af7284-ecee-44e4-b644-5234ca800724.js
@@ -0,0 +1,19 @@
+﻿public int add (int a, int b)
+{
+return a + b;
+}
+
+public int sub(int a, int b)
+{
+return a - b;
+}
+
+public int multiply(int a, int b)
+{
+return a * b;
+}
+
+public int divide (int a, int b)
+{
+return a / b;
+}
